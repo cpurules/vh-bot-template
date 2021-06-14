fda8bae690017c1f40878de95d2ce2a65f65b88c
Charlie U <dzcharlie@gmail.com>
2021-06-14 00:00:17 -0400

    Set prefix to +

--- bot/template_bot.py
+++ bot/template_bot.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.members = True
-bot = commands.Bot(command_prefix='', intents=intents)
+bot = commands.Bot(command_prefix='+', intents=intents)
 bot.remove_command('help') # Will replace this with our custom help command
 
 activity = discord.Activity(type=discord.ActivityType.listening, name='commands')
